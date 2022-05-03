984dea23099cc16d17e0ec65ca5fb6917246caff
Alfredo Gomez <alfretg.c@hotmail.com>
2022-05-03 01:51:08 -0500

    Registro de usuarios sin hash

--- app/src/main/java/com/example/educasen/Registrar.java
+++ app/src/main/java/com/example/educasen/Registrar.java
@@ -1,14 +1,180 @@
 package com.example.educasen;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class Registrar extends AppCompatActivity {
+
+    EditText NombreEt, CorreoEt, ContrasenaEt, ConfirmarContrasenaEt;
+    Button RegistrarUsuario;
+    TextView TengoUnaCuentaTXT;
+
+    // Llamamos a Firebase authentication
+    FirebaseAuth firebaseAuth;
+    ProgressDialog progressDialog;
+
+    // Creamos 4 cadenas de texto para validacion de datos
+    String nombre = " " , correo = " ", password = "" , confirmarpassword = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Registrar");
+        // Propiedades para retroceder y vuelve al mismo activity
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        // Inicializamos todas las vistas
+        NombreEt = findViewById(R.id.NombreEt);
+        CorreoEt = findViewById(R.id.CorreoEt);
+        ContrasenaEt = findViewById(R.id.ContrasenaEt);
+        ConfirmarContrasenaEt = findViewById(R.id.ConfirmarContrasenaEt);
+
+        RegistrarUsuario = findViewById(R.id.RegistrarUsuario);
+        TengoUnaCuentaTXT = findViewById(R.id.TengoUnaCuentaTXT);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(Registrar.this);
+        progressDialog.setTitle("Espere por favor");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        RegistrarUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Metodo Validar
+                ValidarDatos();
+            }
+        });
+
+        TengoUnaCuentaTXT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            // Evento crear cuenta
+            public void onClick(View view) {
+                startActivity(new Intent(Registrar.this, login.class));
+            }
+        });
+    }
+
+    // Creamos el metodo
+
+    private void ValidarDatos () {
+        //Para obtener la informacion
+        nombre = NombreEt.getText().toString();
+        correo = CorreoEt.getText().toString();
+        password = ContrasenaEt.getText().toString();
+        confirmarpassword = ConfirmarContrasenaEt.getText().toString();
+
+        // Condiciones para validar cada campo
+        if (TextUtils.isEmpty(nombre)){
+            Toast.makeText(this, "Ingrese nombre", Toast.LENGTH_SHORT).show();
+        }
+        else if (!Patterns.EMAIL_ADDRESS.matcher(correo).matches()){
+            Toast.makeText(this, "Ingrese correo", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Ingrese contraseña", Toast.LENGTH_SHORT).show();
+
+        }
+        else if (TextUtils.isEmpty(confirmarpassword)){
+            Toast.makeText(this, "Confirme contraseña", Toast.LENGTH_SHORT).show();
+
+        }
+        else if (!password.equals(confirmarpassword)){
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            //Metodo crear cuenta
+            CrearCuenta();
+        }
+    }
+
+    private void CrearCuenta() {
+        progressDialog.setMessage("Creando su cuenta...");
+        progressDialog.show(); //Para que se pueda mostrar el sms
+
+        //Crear un usuario en Firebase
+        firebaseAuth.createUserWithEmailAndPassword(correo, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        //Metodo que permite realizar el registro
+                        GuardarInformacion();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+
+                    //Cuando falle el registro
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss(); //Para que se pueda ocultar
+                Toast.makeText(Registrar.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void GuardarInformacion() {
+        progressDialog.setMessage("Guardando su información");
+        progressDialog.dismiss();
+
+        //Obtener la identificación de usuario actual
+        String uid = firebaseAuth.getUid();
+
+        // Con HashMap enviamos los datos que asignemos desde el dispositivo para despues almacenarlo en la db
+        HashMap<String, String> Datos = new HashMap<>();
+        Datos.put("uid",  uid);
+        Datos.put("correo", correo);
+        Datos.put("nombres", nombre);
+        Datos.put("password", password);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Usuarios");
+        databaseReference.child(uid) // La bd se listen por uid
+                .setValue(Datos)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        progressDialog.dismiss();
+                        Toast.makeText(Registrar.this, "Cuenta creada con éxito", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(Registrar.this, MenuPrincipal.class));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull  Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(Registrar.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
