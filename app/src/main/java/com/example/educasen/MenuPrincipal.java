3a3166ac2fdb9e3704859840271aa242e43d0432
Alfredo Gomez <alfretg.c@hotmail.com>
2022-05-03 03:34:05 -0500

    Cerrar sesion

--- app/src/main/java/com/example/educasen/MenuPrincipal.java
+++ app/src/main/java/com/example/educasen/MenuPrincipal.java
@@ -2,13 +2,45 @@ package com.example.educasen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MenuPrincipal extends AppCompatActivity {
+
+    Button CerrarSesion;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        //Inicializamos el boton y los servicios de firebase
+        CerrarSesion = findViewById(R.id.CerrarSesion);
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser(); //Para obtener el usuario actual
+
+        //Evento al boton
+        CerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Metodo
+                SalirAplicacion();
+            }
+        });
+    }
+
+    private void SalirAplicacion() {
+        //Permite al usuario cerrar sesion
+        firebaseAuth.signOut();
+        startActivity(new Intent(MenuPrincipal.this, MainActivity.class));
+        Toast.makeText(this, "Cerraste sesion exitosamente", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/educasen/PantallaDeCarga.java
+++ app/src/main/java/com/example/educasen/PantallaDeCarga.java
@@ -5,22 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class PantallaDeCarga extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_de_carga);
 
+        //Inicializamos
+        firebaseAuth = FirebaseAuth.getInstance();
+
         // Creamos el metodo que sirve para controlar el tiempo de carga y la redireccion al siguiente activity
         int Tiempo = 3000;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(PantallaDeCarga.this, MainActivity.class));
-                finish();
+                /*startActivity(new Intent(PantallaDeCarga.this, MainActivity.class));
+                finish();*/
+                VerificarUsuario ();
+
             }
         }, Tiempo);
+    }
+
+    private void VerificarUsuario () {
+        // Estamos obteniendo el usuario actual
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+        //Condicion para saber si el usuario a iniciado sesion o no
+        if (firebaseUser == null) {
+            startActivity(new Intent(PantallaDeCarga.this, MainActivity.class));
+            finish();
+        } else {
+            startActivity(new Intent(PantallaDeCarga.this, MenuPrincipal.class));
+            Toast.makeText(this, "Iniciando sesion", Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 }
